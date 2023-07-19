c184f875535f4e23ebda1ca365bdb4d1bfe07b83
Abhi <Bh00fie@gmail.com>
2023-07-19 21:57:24 +0100

    imported boostrap library in index.js as issue: Element type is invalid: expected a string fixed by npm install --save-dev @babel/plugin-proposal-private-property-in-object

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
