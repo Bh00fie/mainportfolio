58cb2331fa93a03edd212508810d056797fcea5c
Abhi <Bh00fie@gmail.com>
2023-07-22 00:20:53 +0100

    added styling to home page by rearrenging the position of the elements through the page view, styling such as shapes/palette/icons/etc.. still needs to be added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,19 +6,24 @@ function Home() {
         <div id='homePage'>
             <div id='profileSection'>
                 <img className="profileCard" id='photoProfile' src='https://placehold.co/400' alt='photoProfile'/>    
-                <div className='profileCard' id='profileDescription'>
-                    <h3 id='nameProfile'> Abhinandan Thour </h3>
-                    <h4 id='jobProfile'> Front-End Developer</h4>
-                    <div id='socialProfile'>
-                        <a id='GitHub' className='socialLink' href='https://github.com/Bh00fie' target='_blank' >GitHub</a>
-                        <a id="LinkedIn" className='socialLink' href='https://www.linkedin.com/in/abhinandanthour' target='_blank'>LinkedIn</a>
+                <div>
+                    <div className='profileCard' id='profileDescription'>
+                        <h3 id='nameProfile'> Abhinandan Thour </h3>
+                        <h4 id='jobProfile'> Front-End Developer</h4>
+                        <div id='socialProfile'>
+                            <a id='GitHub' className='socialLink' href='https://github.com/Bh00fie' target='_blank' >GitHub</a>
+                            <a id="LinkedIn" className='socialLink' href='https://www.linkedin.com/in/abhinandanthour' target='_blank'>LinkedIn</a>
+                        </div>
+                    </div>
+                    <div className='myCV'>
+                        <a href="./abhinandanthour.pdf" download="abhinandanthourCV.pdf">
+                            <button id='cvButton'>CV</button>
+                        </a>
                     </div>
                 </div>
-                <div className='myCV'>
-                     <a href='#'><h2> CV </h2></a>
-                </div>
+
             </div>
-            <div id='profileDescription'>
+            <div id='profileSummary'>
                 <h1 className='profileInformation' id='profileSummaryTitle'> Who's Abhi?</h1>
                 <p className='profileInformation' id='profileSummaryDescription'>I'm an Aerospace and Mechanical Engineering student at the University of Southampton. With a background in Italy, Ireland, and the UK, I bring a diverse perspective. My passion for technology and improving lives led me to pursue Mechanical Engineering, but I've realized the increasing influence of software and now seek to transition into this field. Explore my portfolio to learn more about my academic achievements and projects as I embark on this exciting journey into software.</p>
             </div>
