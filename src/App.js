90c0207b831d8d9895823126aadce61736fbd93e
Abhi <Bh00fie@gmail.com>
2023-07-29 01:39:50 +0100

    added more progress to keep footer at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
-
-// Importing pages/element from the component folder, all "Names" are function in those file there are being exported to the App.js file
 import NavTabs from '../src/components/NavTabs.js';
 import Home from '../src/components/pages/Home.js';
 import Projects from '../src/components/pages/Projects.js';
@@ -10,23 +8,22 @@ import About from './components/pages/Aboutme.js';
 import Contactme from '../src/components/pages/Contactme.js';
 import Footer from '../src/components/Footer';
 import FixedCV from '../src/components/quickCV.js';
-
-
-// We import the main Style.css file
-import './components/style.css'
+import './components/style.css';
 
 function App() {
   return (
     <Router>
-      <NavTabs/>
+      <NavTabs />
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="projects" element={<Projects />} />
           <Route path="aboutme" element={<About />} />
           <Route path="contactme" element={<Contactme />} />
-        </Routes>  
-      <Footer/>
-      <FixedCV/>
+        </Routes>
+        <Footer />
+        <FixedCV />
+      </div>
     </Router>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -35,7 +35,7 @@ function Projects() {
             {/* Code to create a grid of cards copied from React-Bootstrap */}
             <h1 id='projectTitle' className='title'>Projects:</h1>
             <div id='projectGrid'>
-            <Row xs={1} md={2} className="g-4">
+            <Row xs={1} md={2} className="g-5">
                 {/* g-4 stands for the spacing between cards when in a row */}
                 {/* Creating an array of cards and give them an index value idx */}
                 {cardInformation.map((card, idx) => (
