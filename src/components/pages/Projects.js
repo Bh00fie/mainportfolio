fabffc47dfb9c614e92730f6281011fc6c2a0cc6
Abhi <Bh00fie@gmail.com>
2023-08-01 20:59:15 +0100

    added new project to portfolio

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,13 +11,13 @@ function Projects() {
         {
             title: "Workout Kitchen",
             image: "https://github.com/Allen-EC/workout_kitchen/raw/main/assets/images/screenshot.png",
-            text: "Version 2 in progress",
+            text: "Webpage to help improve your overall health! Using API the user can look for recipes based on what they have in stock and also select a an exercise based on the difficulty and type!",
             linkProject: "https://allen-ec.github.io/workout_kitchen/"
         },  
         {
-            title: "Health Tracker",
-            image: "https://ak.picdn.net/shutterstock/videos/1012114871/thumb/1.jpg?ip=x480",
-            text: "Work In Progress",
+            title: "Cover Letter Generator with AI!",
+            image: "https://cdn-thumbnails.huggingface.co/social-thumbnails/spaces/nouamanetazi/cover-letter-generator.png",
+            text: "Project that allows the user to upload their CV and the Job description to get a quick tailored cover letter using ChatGPT API!",
         },  
         {
             title: "Mechanical Engineering Project",
