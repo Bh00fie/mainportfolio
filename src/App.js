d372adc8d86af7b98589d556c2b306169ada7132
Abhinandan Thour <Bh00fie@gmail.com>
2025-01-01 23:50:02 +0000

    clean up of images and sections that were not needed such as the one about my past and my mechanical engineering experience

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/pages/1 - Home.js';
 import WhosAbhi from './components/pages/2 - WhosAbhi.js';
 import Projects from './components/pages/3 - Projects.js';
-// import About from './components/pages/4 - Aboutme.js';
-import Contactme from './components/pages/5 - Contactme.js';
-import Footer from './components/pages/6 - Footer.js';
+import Contactme from './components/pages/4 - Contactme.js';
+import Footer from './components/pages/5 - Footer.js';
 // import FixedCV from './components/pages/quickCV.js';
 // import ScrollDown from '../src/components/pages/ScrollDownFunction.js';
 
